dac460a4a782b837238fddc334e7c50083b786e5
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2023-11-27 00:05:13 +0100

    Create HTTP Responses trait

---
+++ app/Traits/HttpResponses.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Traits;
+
+trait HttpResponses
+{
+    /**
+     * HTTP Response for success
+     *
+     * @param string $dataLabel
+     * @param any $dataValue
+     * @param integer $code
+     * @return void
+     */
+    protected function success($dataLabel, $dataValue, $code = 200)
+    {
+        return response()->json(
+            [
+                'status' => 'sucess',
+                $dataLabel => $dataValue,
+            ],
+            $code
+        );
+    }
+
+    /**
+     * HTTP Response for error
+     *
+     * @param string $message
+     * @param integer $code
+     * @return void
+     */
+    protected function error($message, $code)
+    {
+        return response()->json(
+            [
+                'status' => 'error',
+                'message' => $message,
+            ],
+            $code
+        );
+    }
+}
