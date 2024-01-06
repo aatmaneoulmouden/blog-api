3316ca0b83542fc58f910a8bd21817fb5f699334
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-01-06 18:22:22 +0100

    Add user_id to posts table + setup relationships between User and Post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,11 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'short_description',
+        'content',
+        'featured_image',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 
     public function categories(): BelongsToMany
     {
--- app/Models/User.php
+++ app/Models/User.php
@@ -46,6 +46,11 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
 
     public function categories(): HasMany
     {
