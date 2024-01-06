957d4c007f833551efcf4775b77fbaf66c59ccfd
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-01-06 18:33:27 +0100

    Add slug to posts table

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
@@ -16,33 +17,45 @@ class PostController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        // Get authenticated user
+        $user = $request->user();
+
+        // Check if user exist
+        if (!$user) {
+            return $this->error('User not found.', 422);
+        }
+
+        // Check if user already have a post with the same title
+        $postTitle = $request->input('title');
+        $postTitle = $user->posts()->where('title', $postTitle)->first();
+
+        if ($postTitle) {
+            return $this->error('You already have a post with the same title.', 422);
+        }
+
+        // Create post
+        $post = Post::create([
+            'user_id' => $user->id,
+            'user_id' => $user->id,
+            'name' => $postTitle,
+            'slug' => Str::slug($postTitle),
+        ]);
+
+        // Get tag data
+        $tagData = new TagResource($tag);
+
+        // Return http response
+        return $this->success('category', $tagData, 201);
     }
 
     /**
      * Display the specified resource.
      */
     public function show(Post $post)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Post $post)
     {
         //
     }
--- database/migrations/2023_12_03_201602_create_posts_table.php
+++ database/migrations/2023_12_03_201602_create_posts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('title');
+            $table->string('slug');
             $table->text('short_description')->nullable();
             $table->longText('content');
             $table->string('featured_image')->nullable();
