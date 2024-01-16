f3cab8d38a3c78fe02f28e1ce532b691fbd91d30
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-01-16 08:48:45 +0100

    Add super_admin column to users table and seed the db with a Super Admin user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('super_admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +13,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Create super admin
+        \App\Models\User::factory()->create([
+            'first_name' => 'Super',
+            'last_name' => 'Admin',
+            'username' => 'useradmin',
+            'email' => 'useradmin@email.com',
+            'password' => Hash::make('Pa$$w0rd'),
+            'super_admin' => true,
+        ]);
+
+        // Seed database
         \App\Models\User::factory(10)->create();
         \App\Models\Category::factory(50)->create();
         \App\Models\Tag::factory(100)->create();
