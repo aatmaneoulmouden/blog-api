b17ddc59b3160f5cfe31363a54f8f15155b14907
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-02-02 20:44:25 +0100

    Remove relationship between categorie and user

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,16 +47,11 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-    
+
     public function posts(): HasMany
     {
         return $this->hasMany(Post::class);
     }
-
-    // public function categories(): HasMany
-    // {
-    //     return $this->hasMany(Category::class);
-    // }
 
     public function tags(): HasMany
     {
